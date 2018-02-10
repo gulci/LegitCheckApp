356b8912d0dd7d31a981748ff8ca3f5036175ffd
Mario Muniz <mario@mariomuniz.com>
2018-02-10 14:21:36 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Legit Check App
+
+This application is designed to facilitate checking the legitimacy of fashion items. For more information, please visit the [official website.](https://chdaniel.com/legit-check-app/)
---
+++ app/Home.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+type Props = {};
+export default class Home extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to the Legit Check App!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
